d14c5e97bd52935d454cd3613f73a54ed8ba15d2
yesm4n <mrylliramadani@gmail.com>
2023-09-28 21:48:45 +0200

    project complete

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 
-const hourglass = document.querySelector(".lds-hourglass");
-const container = document.querySelector(".container");
-const fullscreen = document.querySelector(".full-screen");
-const gallery = document.querySelector(".gallery");
-const date = document.querySelector(".date");
-const imagesLength = document.querySelector(".images-length");
+const hourglass = document.querySelector('.lds-hourglass');
+const container = document.querySelector('.container');
+const fullscreen = document.querySelector('.full-screen');
+const gallery = document.querySelector('.gallery');
+const date = document.querySelector('.date');
+const imagesLength = document.querySelector('.images-length');
 
-fullscreen.classList.add("hidden");
-container.classList.add("hidden");
+fullscreen.classList.add('hidden');
+container.classList.add('hidden');
 
 let index = 1;
 
@@ -16,33 +16,33 @@ let index = 1;
 // IMAGES SECTION
 //
 
-const emptyArr = Array.from({ length: 23 }, (_, i) => i + 1).map((num) => num);
+const emptyArr = Array.from({ length: 23 }, (_, i) => i + 1).map(num => num);
 
 // Creating new images based on the array above length
 function handleCreateImages() {
   emptyArr.forEach((arr, i) => {
-    const div = document.createElement("div");
-    const img = document.createElement("img");
-    const span = document.createElement("span");
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    const span = document.createElement('span');
 
     const wideDiv = [2, 7, 15, 18];
-    if (wideDiv.includes(arr)) div.classList.add("card-wide");
+    if (wideDiv.includes(arr)) div.classList.add('card-wide');
 
-    div.classList.add("card");
-    img.setAttribute("src", `./images/${arr}.png`);
-    span.textContent = i < 9 ? "0" + (i + 1) : i + 1;
+    div.classList.add('card');
+    img.setAttribute('src', `./images/${arr}.png`);
+    span.textContent = i < 9 ? '0' + (i + 1) : i + 1;
 
     div.appendChild(img);
     div.appendChild(span);
     gallery.appendChild(div);
 
-    img.addEventListener("click", () => {
-      container.classList.add("hidden");
-      fullscreen.classList.remove("hidden");
+    img.addEventListener('click', () => {
+      container.classList.add('hidden');
+      fullscreen.classList.remove('hidden');
 
       index = i + 1;
-      mainImg.setAttribute("src", `./images/${index}.png`);
-      sideImg.setAttribute("src", `./images/${index + 1}.png`);
+      mainImg.setAttribute('src', `./images/${index}.png`);
+      sideImg.setAttribute('src', `./images/${index + 1}.png`);
     });
   });
 }
@@ -65,28 +65,28 @@ imagesLength.textContent = `(${emptyArr.length})`;
 // FULL-SCREEN SECTION
 //
 
-const viewGallery = document.querySelector(".view-gallery");
-const mainImg = document.querySelector(".main-img");
-const sideImg = document.querySelector(".side-img");
-const prevButton = document.querySelector(".btn-prev");
-const nextButton = document.querySelector(".btn-next");
+const viewGallery = document.querySelector('.view-gallery');
+const mainImg = document.querySelector('.main-img');
+const sideImg = document.querySelector('.side-img');
+const prevButton = document.querySelector('.btn-prev');
+const nextButton = document.querySelector('.btn-next');
 
 // Fullscreen
 const handleFullscreenOn = () => {
-  container.classList.add("hidden");
-  fullscreen.classList.remove("hidden");
+  container.classList.add('hidden');
+  fullscreen.classList.remove('hidden');
 };
 
 // Remove fullscreen
 const handleFullscreenOff = () => {
-  container.classList.remove("hidden");
-  fullscreen.classList.add("hidden");
+  container.classList.remove('hidden');
+  fullscreen.classList.add('hidden');
 };
 
 // Loading
 const handleLoading = () => {
-  hourglass.classList.add("hidden");
-  container.classList.remove("hidden");
+  hourglass.classList.add('hidden');
+  container.classList.remove('hidden');
 };
 
 const maxIndex = emptyArr.length;
@@ -99,8 +99,8 @@ const handleNextImages = () => {
     index = index + 1;
   }
 
-  mainImg.setAttribute("src", `./images/${index}.png`);
-  sideImg.setAttribute("src", `./images/${index + 1}.png`);
+  mainImg.setAttribute('src', `./images/${index}.png`);
+  sideImg.setAttribute('src', `./images/${index + 1}.png`);
 };
 
 // Previous button
@@ -111,27 +111,60 @@ const handlePrevImages = () => {
     index = index - 1;
   }
 
-  mainImg.setAttribute("src", `./images/${index}.png`);
-  sideImg.setAttribute("src", `./images/${index + 1}.png`);
+  mainImg.setAttribute('src', `./images/${index}.png`);
+  sideImg.setAttribute('src', `./images/${index + 1}.png`);
 };
 
 // Mouse wheel => horizontally
-const wheelFunc = (e) => {
+const wheelFunc = e => {
   gallery.scrollLeft += e.deltaY;
 };
 
 // Keyboard events
-const handleKeypress = (e) => {
-  if (e.key === "ArrowLeft") handlePrevImages();
-  if (e.key === "ArrowRight") handleNextImages();
-  if (e.key === "Escape") handleFullscreenOff();
+const handleKeypress = e => {
+  if (e.key === 'ArrowLeft') handlePrevImages();
+  if (e.key === 'ArrowRight') handleNextImages();
+  if (e.key === 'Escape') handleFullscreenOff();
 };
 
-window.addEventListener("load", handleLoading);
-window.addEventListener("wheel", wheelFunc);
-document.addEventListener("keydown", handleKeypress);
+window.addEventListener('load', handleLoading);
+window.addEventListener('wheel', wheelFunc);
+document.addEventListener('keydown', handleKeypress);
 
-viewGallery.addEventListener("click", handleFullscreenOn);
-mainImg.addEventListener("click", handleFullscreenOff);
-prevButton.addEventListener("click", handlePrevImages);
-nextButton.addEventListener("click", handleNextImages);
+viewGallery.addEventListener('click', handleFullscreenOn);
+mainImg.addEventListener('click', handleFullscreenOff);
+prevButton.addEventListener('click', handlePrevImages);
+nextButton.addEventListener('click', handleNextImages);
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(function (card) {
+  card.addEventListener('mouseenter', function () {
+    this.classList.add('active');
+    const prevCard = this.previousElementSibling;
+    const nextCard = this.nextElementSibling;
+
+    if (prevCard) {
+      prevCard.classList.add('shrink-left');
+    }
+
+    if (nextCard) {
+      nextCard.classList.add('shrink-right');
+    }
+  });
+
+  card.addEventListener('mouseleave', function () {
+    this.classList.remove('active');
+    const prevCard = this.previousElementSibling;
+    const nextCard = this.nextElementSibling;
+
+    if (prevCard) {
+      prevCard.classList.remove('shrink-left');
+    }
+
+    if (nextCard) {
+      nextCard.classList.remove('shrink-right');
+    }
+  });
+});
+//   });
